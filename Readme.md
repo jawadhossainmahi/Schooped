ff221bf631876fb99e7eb23b714e62e53df36469
Jawad Hossain Mahi <jawadhossainmahi@gmail.com>
2024-07-10 04:29:44 +0600

    complete

---
+++ Readme.md
@@ -0,0 +1,19 @@
+# Scooped Website
+
+Welcome to the official GitHub repository for the "Scooped" movie website. This repository contains all the files needed to run and view the "Scooped" website, including the main `index.html` file and associated assets.
+
+## Overview
+
+The "Scooped" website provides information about the thrilling adventure film "Scooped." It features a detailed overview of the film, key characters, storyline, and access to the official trailer. The website is designed to be user-friendly and visually engaging, offering an immersive experience for visitors.
+
+## How to View the Website
+
+### Prerequisites
+
+- A web browser (Chrome, Firefox, Safari, etc.)
+
+### Steps
+
+1. **Clone the Repository**
+   ```bash
+   git clone https://github.com/yourusername/scooped-website.git
---
+++ assets/js/scripts.js
@@ -0,0 +1,46 @@
+let tab_buttons = document.getElementsByClassName("tab-buttons")[0];
+if (tab_buttons) {
+    let btn = tab_buttons.querySelectorAll(".btn");
+    btn.forEach((e, index) => {
+
+        e.addEventListener("click", () => {
+            btn.forEach(e2 => {
+                if (e2.classList.contains("btn-dark")) {
+                    e2.classList.add("btn-outline-dark")
+                    e2.classList.remove("btn-dark")
+                    
+                }
+            })
+            if (e.classList.contains("btn-outline-dark")) {
+                e.classList.add("btn-dark");
+                e.classList.remove("btn-outline-dark");
+                let tab_id = e.getAttribute("data-tab-id")
+                change_tab_section("tab_box",tab_id)
+            }
+
+        })
+
+    })
+}
+
+function change_tab_section(tab_class, tab_id) {
+    let tab_element = document.querySelectorAll("." + tab_class);
+    tab_element.forEach(e => {
+        if (!e.classList.contains("d-none")) {
+            e.classList.add("d-none")
+        }
+    })
+    if(document.getElementById(tab_id).classList.contains("d-none")){
+        document.getElementById(tab_id).classList.remove("d-none");
+    }
+}
+
+
+// copyright year
+
+const d = new Date();
+let year = d.getFullYear();
+let footer_current_year = document.querySelector("#footer .footer_current_year");
+if (footer_current_year) {
+  footer_current_year.innerText = year
+}
