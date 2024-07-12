efd3953c06d6c7ff637afbe50296be3c2186fede
Jawad Hossain Mahi <jawadhossainmahi@gmail.com>
2024-07-13 02:26:16 +0600

    complete

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -8,14 +8,14 @@ if (tab_buttons) {
                 if (e2.classList.contains("btn-dark")) {
                     e2.classList.add("btn-outline-dark")
                     e2.classList.remove("btn-dark")
-                    
+
                 }
             })
             if (e.classList.contains("btn-outline-dark")) {
                 e.classList.add("btn-dark");
                 e.classList.remove("btn-outline-dark");
                 let tab_id = e.getAttribute("data-tab-id")
-                change_tab_section("tab_box",tab_id)
+                change_tab_section("tab_box", tab_id)
             }
 
         })
@@ -30,7 +30,7 @@ function change_tab_section(tab_class, tab_id) {
             e.classList.add("d-none")
         }
     })
-    if(document.getElementById(tab_id).classList.contains("d-none")){
+    if (document.getElementById(tab_id).classList.contains("d-none")) {
         document.getElementById(tab_id).classList.remove("d-none");
     }
 }
@@ -42,5 +42,26 @@ const d = new Date();
 let year = d.getFullYear();
 let footer_current_year = document.querySelector("#footer .footer_current_year");
 if (footer_current_year) {
-  footer_current_year.innerText = year
+    footer_current_year.innerText = year
 }
+
+
+$(document).ready(function () {
+    function changeImageSrc() {
+        if ($(window).width() < 768) { // Mobile view (you can adjust the width threshold as needed)
+            $('.banner-image').attr('src', './assets/images2/ScoopedBannerv2.png');
+        }
+        if ($(window).width() > 768) {
+            // PC view
+            $('.banner-imagess').attr('src', './assets/images2/ScoopedBanner.png');
+        }
+    }
+
+    // Call function on page load
+    changeImageSrc();
+
+    // Call function on window resize
+    $(window).resize(function () {
+        changeImageSrc();
+    });
+});
